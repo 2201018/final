fdfea868e368636bd1ddc064c3bcfecbcb6247c7
2201018 <148177579+2201018@users.noreply.github.com>
2024-01-11 12:45:32 +0900

    a

--- src/db-connect.php
+++ src/db-connect.php
@@ -1,5 +1,5 @@
 <?php
-    const SERVER = 'mysql218.phy.lolipop.lan';
+    const SERVER = 'mysql220.phy.lolipop.lan';
     const DBNAME = 'LAA1517324-final';
     const USER = 'LAA1517324';
     const PASS = 'Pass0919';
